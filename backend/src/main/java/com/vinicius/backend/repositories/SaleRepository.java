fd54ca72bdc6c919214443b5bf5f490b48384930
Vinicius Gomes <viniplay0599@gmail.com>
2022-09-11 19:30:27 -0300

    feat: implements query by date

--- backend/src/main/java/com/vinicius/backend/repositories/SaleRepository.java
+++ backend/src/main/java/com/vinicius/backend/repositories/SaleRepository.java
@@ -1,7 +1,14 @@
 package com.vinicius.backend.repositories;
 
 import com.vinicius.backend.models.Sales;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDate;
 
 public interface SaleRepository extends JpaRepository<Sales, Long> {
+    @Query("SELECT obj FROM Sales obj WHERE obj.date BETWEEN :min AND :max ORDER BY obj.amount DESC")
+    Page<Sales> findSales(LocalDate min, LocalDate max, Pageable pageable);
 }
--- backend/src/main/java/com/vinicius/backend/service/SaleService.java
+++ backend/src/main/java/com/vinicius/backend/service/SaleService.java
@@ -3,16 +3,25 @@ package com.vinicius.backend.service;
 import com.vinicius.backend.models.Sales;
 import com.vinicius.backend.repositories.SaleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 @Service
 public class SaleService {
     @Autowired
     private SaleRepository repository;
 
-    public List<Sales> findSales() {
-        return repository.findAll();
+    public Page<Sales> findSales(String minDate, String maxDate, Pageable pageable) {
+        LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+
+        LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+        LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+
+        return repository.findSales(min, max, pageable);
     }
 }
