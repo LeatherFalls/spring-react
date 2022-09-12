7a548414400dcd74824953753ef5d7802cd8601b
Vinicius Gomes <viniplay0599@gmail.com>
2022-09-11 22:46:53 -0300

    style: add toastify styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import Header from './components/Header';
 import SalesCard from './components/SalesCard';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
     <div className='App'>
+      <ToastContainer />
       <Header />
       <main>
         <section id="sales">
--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import vector from '../../assets/images/vector.svg';
 import { BASE_URL } from '../../utils/request';
+import { toast } from 'react-toastify';
 import './styles.css';
 
 type Props = {
@@ -8,16 +9,16 @@ type Props = {
 } 
 
 function handleClick(saleId: number) {
-  axios.get(`${BASE_URL}sales/${saleId}/notification`)
-    .then((response) => {
-      console.log(response);
+  axios.get(`${BASE_URL}/sales/${saleId}/notification`)
+    .then((_response) => {
+      toast('Notification sent successfully');
     });
 }
 
 function NotificationButton({ saleId }: Props) {
   return (
     <div className='dsmeta-red-btn' onClick={ () => handleClick(saleId) }>
-    <img src={ vector } alt='Notificar' />
+    <img src={ vector } alt='Notifify' />
   </div>
   );
 }
